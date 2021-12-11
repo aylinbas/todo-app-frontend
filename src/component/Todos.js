24bbf16aec2ab5788d3d6e3eadd56ea70bd68ef2
aylinbas <ayln.bas.50@hotmail.com>
2021-12-11 16:19:12 +0300

    test

---
+++ src/component/Todos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Todos() {
+  return <div data-testid="todo-element">Hello world!</div>;
+}
+
+export default Todos;
---
+++ src/component/Todos.test.js
@@ -0,0 +1,8 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import Todos from "./Todos";
+
+test("should render Todos component", () => {
+  render(<Todos />);
+  const todoElement = screen.getByTestId("todo-element");
+  expect(todoElement).toBeInTheDocument();
+});
