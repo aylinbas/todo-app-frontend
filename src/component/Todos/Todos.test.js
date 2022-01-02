43d0a688a14b2e38aa1a8e578ff0ecfa8c517b6f
aylinbas <ayln.bas.50@hotmail.com>
2022-01-02 15:13:56 +0300

    config.yml circle ci

--- src/component/Todos/Todos.test.js
+++ src/component/Todos/Todos.test.js
@@ -1,11 +1,13 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Todos from "./Todos";
 
 test("should render Todos component", () => {
   render(<Todos />);
-  const todoElement = screen.getByTestId("todo-element");
-  expect(todoElement).toBeInTheDocument();
-  const todoTitle = screen.getByTestId("todo-title");
-  expect(todoTitle).toBeInTheDocument();
-  expect(todoTitle).toHaveTextContent("Add New Todo");
+  setTimeout(() => {
+    const todoElement = screen.getByTestId("todo-element");
+    expect(todoElement).toBeInTheDocument();
+    const todoTitle = screen.getByTestId("todo-title");
+    expect(todoTitle).toBeInTheDocument();
+    expect(todoTitle).toHaveTextContent("Add New Todo");
+  }, 1500);
 });
